f5331d84121fba838b9fffadec7daca98b2456c1
Blaz <blaz.kosica@gmail.com>
2022-06-19 22:49:51 +0000

    Add some javascript to fetch random quotes

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
-let quoteText = document.querySelector(".quote-text");
+const quoteText = document.querySelector(".quote-text"); // #Variable for Quote
+generateQuoteBtn = document.querySelector(".generate-quote");
 
-console.log(quoteText.innerText)
+//Random Quote function
+function randomQuote(){
+    //fetch random quote and transform it into object "guote text"
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result =>{
+        console.log(result)
+    })
+};
+
+generateQuoteBtn = addEventListener('click', randomQuote); // for generate Quote bottom
+
